e10758935972f38016cc16f5ba22f5e3537dd5a7
JuJiiz <ju.jiiz.1579@gmail.com>
2022-07-06 17:47:20 +0700

    update login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -8,16 +8,86 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _userNameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  _toggleLogin() {
+    final username = _userNameController.text;
+    final password = _passwordController.text;
+
+    showDialog(
+      context: context,
+      builder: (BuildContext dialogContext) => Dialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+          color: Colors.white,
+          child: Column(
+            children: [
+              Text('username: $username'),
+              Text('password: $password'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'JuJiiz Login',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 32,
-          ),
+    return Scaffold(
+      body: Container(
+        color: Colors.amberAccent,
+        child: Column(
+          children: [
+            const Text(
+              'JuJiiz Login',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 56,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 24),
+            TextField(
+              controller: _userNameController,
+              textAlign: TextAlign.center,
+              maxLines: 1,
+              keyboardType: TextInputType.text,
+              decoration: const InputDecoration(
+                hintText: 'UserName',
+              ),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              controller: _passwordController,
+              textAlign: TextAlign.center,
+              maxLines: 1,
+              obscureText: true,
+              keyboardType: TextInputType.text,
+              decoration: const InputDecoration(
+                hintText: 'Password',
+              ),
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: _toggleLogin,
+              style: ElevatedButton.styleFrom(
+                primary: Colors.pink,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: const Text(
+                'Login',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
